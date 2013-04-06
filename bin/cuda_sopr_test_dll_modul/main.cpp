f101cae228016172c8f0ebdb94a14e9fe1791cf5
aulyanov <zbrg@mail.ru>
2013-04-06 14:40:00 +0700

    slau

--- bin/cuda_sopr_test_dll_modul/main.cpp
+++ bin/cuda_sopr_test_dll_modul/main.cpp
@@ -6,9 +6,6 @@
 #include <stdlib.h>
 #include "windows.h"
 
-#include <stdio.h>
-#include <conio.h>
-#include <time.h>
 
 #pragma hdrstop
 
@@ -21,64 +18,82 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+  rett symAo[7*7] = {
+		9.0 ,  0.0 , 0.0 , 3.0 , 1.0 , 0.0 , 1.0 ,
+		0.0 ,  11.0, 2.0 , 1.0 , 5.0 , 0.0 , 2.0 ,
+		0.0 ,  2.0 , 10.0 , 2.0 , -7.0 , 0.0 , 0.0 ,
+		3.0 ,  1.0 ,  2.0 ,  9.0 , 1.0 , 0.0 , 0.0 ,
+		1.0 ,  5.0 ,  -7.0,  1.0 ,  12.0 , 0.0 , 1.0 ,
+		0.0 ,  0.0 ,  0.0 ,  0.0 ,  0.0  , 8.0 ,  0.0 ,
+		1.0 ,  2.0 ,  0.0 ,  0.0 ,  1.0  , 0.0 ,  80.0
+  };
+  rett symA[7*7] = {
+		9.0 ,  0.0 , 0.0  ,  0.0 ,   0.0 , 0.0 ,  0.0 ,
+	        0.0 ,  11.0, 2.0  ,  1.0 ,   0.0 , 0.0 ,  0.0 ,
+		0.0 ,  2.0 , 10.0 ,  2.0 ,  -7.0 , 0.0 ,  0.0 ,
+		0.0 ,  1.0 ,  2.0 ,  9.0 ,  1.0  , 0.0 ,  0.0 ,
+		0.0 ,  0.0 , -7.0 ,  1.0 ,  12.0 , 0.0 ,  0.0 ,
+		0.0 ,  0.0 ,  0.0 ,  0.0 ,   0.0 , 8.0 ,  0.0 ,
+		0.0 ,  0.0 ,  0.0 ,  0.0 ,   0.0 , 0.0 , 80.0
+  };
+  rett ssymA[] = {
+		0.0 ,  0.0 , 9.0 ,
+                0.0 ,  0.0 , 11.0 ,
+                0.0 ,  2.0 , 10.0 ,
+                1.0 ,  2.0 , 9.0 ,
+               -7.0 ,  1.0 , 12.0 ,
+                0.0 ,  0.0 , 8.0 ,
+                0.0 ,  0.0 , 80.0
+  };
 
   float time = 0;
 
-  typedef float (__stdcall *testProcedureType) (rett *a, rett *b, rett *c, const countt N);
+  typedef float (__stdcall *testProcedureType) (rett *matrixA, rett *vectorB, rett *vectorX, const countt N, const countt B, rett eps);
   testProcedureType testProcedure;
   HMODULE lib = NULL;
   lib = LoadLibrary("../sopr.dll");
   if(lib){
     std::cerr << "Load libr\n";
     
-    testProcedure = (testProcedureType)GetProcAddress(lib,"_matrixOnMatrixMultiply@16");
-
+    testProcedure = (testProcedureType)GetProcAddress(lib,"_methodConjugateGradientForBandMatrix@24");
     if (testProcedure){
       std::cerr << "Find func\n";
 
       //INIT DATA BLOCK
-      for (int NN = 100; NN < 10000; NN+=100){
-      countt N = NN;//1000;//10100;
+      for (countt NN = 100; NN < 3100; NN+=100){
+
       
-      rett *a1 = new rett[N*N];
-      rett *b1 = new rett[N*N];
-      rett *c1 = new rett[N*N];
+      countt N = NN;
+
+      countt B = 1;
+      //std::cerr << "N = " << N << endl;
+      rett *x = new rett[N];
+      rett *b = new rett[N];
+      rett *pa = new rett[N*(B + 1)];
+      rett eps = 0.00001;
 
       for (int i = 0; i < N; i++ ){
-        for (int j = 0; j < N; j++){
-          c1[i*N + j] = -101;
-          a1[i*N + j] = 1;
-          b1[i*N + j] = 1;
+        if (i < 1){
+          pa[0] = 0;
+          pa[1] = 2;
+        }else{
+          pa[i*(B+1)] = -1;
+          pa[i*(B+1)+1] = 2;
         }
+        //cout << "pa0:" << pa[i*(B+1)] << " pa1:" << pa[i*(B+1)+1] << endl;
+        x[i] = 0.5;
+        b[i] = N/(i + 1);
       }
-      //-----------------------
-unsigned long t=clock();
-      cout << "N = " << N << "; CPU:";
-      for (int i = 0; i < N; i++){
-        for (int j = 0; j < N; j ++){
-          c1[i*N + j] = 0;
-          for (int k = 0; k < N; k++){
-            c1[i*N + j] += a1[i*N + k]*b1[k*N + j];
-          }
-        }
-      }
-      time = clock()-t;
-      std::cerr << "timeout (ms):" << time << "; ";
+      //END OF INIT DATA BLOCK
 
-      cout << "GPU:";
-        time = testProcedure(a1,b1,c1,N);
-      std::cerr << "timeout (ms):" << time << std::endl;
-      delete [] a1;
-      delete [] b1;
-      delete [] c1;
+      time = testProcedure(pa,b,x,N,B,eps);
+
+      std::cerr << N << "->" << time << std::endl;
+      delete [] pa;
+      delete [] b;
+      delete [] x;
+      
       }
-      /*
-      for (int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
-          cout << c1[i*N + j] << ";";
-        }
-        cout << endl;
-      } */
     }
   }
   FreeLibrary(lib);
